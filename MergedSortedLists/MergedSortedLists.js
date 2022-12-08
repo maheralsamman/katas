7e83f6b1e53fe3ec9884654f607017e189193a28
Maher Alsamman <56821558+maheralsamman@users.noreply.github.com>
2022-12-08 20:09:43 +0100

    Update MergedSortedLists.js

--- MergedSortedLists/MergedSortedLists.js
+++ MergedSortedLists/MergedSortedLists.js
@@ -12,19 +12,17 @@
  * @return {ListNode}
  */
 
- var mergeTwoLists = function (list1, list2) {
-  // Check if either of the lists is null 
+ let mergeTwoLists = function (list1, list2) {
   if (!list1) {
       return list2;
   }
   if (!list2) {
       return list1;
   }
-  // Head of the new linked list - this is the head of the resultant list
+  
   let head = null;
-  // Reference of head which is null at this point
   let temp = head;
-  // Choose head which is smaller of the two lists
+  
   if (list1.val < list2.val) {
       temp = head = new ListNode(list1.val);
       list1 = list1.next;
@@ -32,7 +30,7 @@
       temp = head = new ListNode(list2.val);
       list2 = list2.next;
   }
-  // Loop until any of the list becomes null
+  
   while (list1 && list2) {
       if (list1.val < list2.val) {
           temp.next = new ListNode(list1.val);
@@ -44,13 +42,12 @@
           temp = temp.next;
       }
   }
-  // Add all the nodes in list1, if remaining
+  
   while (list1) {
       temp.next = new ListNode(list1.val);
       list1 = list1.next;
       temp = temp.next;
   }
-  // Add all the nodes in list2, if remaining
   while (list2) {
       temp.next = new ListNode(list2.val);
       list2 = list2.next;
